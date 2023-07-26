060b7f80c437fad2eeaa639cea9c9df634064ffc
Brittney Trinh <brittneytrinh@g.ucla.edu>
2023-07-26 14:13:06 -0700

    fix typescript

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,19 @@
     index.ts
     Javascript i/o interactive
 */
-const readline = require("readline");
+// const readline = require("readline");
 
-const MANDATES_CSV =
-  "https://raw.githubusercontent.com/ParkingReformNetwork/mandates-map/main/map/trimmed_map_data.csv";
-const CITIES_JSON =
-  "https://raw.githubusercontent.com/ParkingReformNetwork/parking-lot-map/main/data/score-cards.json";
+// const MANDATES_CSV =
+//   "https://raw.githubusercontent.com/ParkingReformNetwork/mandates-map/main/map/trimmed_map_data.csv";
+// const CITIES_JSON =
+//   "https://raw.githubusercontent.com/ParkingReformNetwork/parking-lot-map/main/data/score-cards.json";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-rl.question("Guess the percentage: ", function (percent) {
-  console.log(`${percent} was guessed.\n`);
-  rl.close();
-});
+// rl.question("Guess the percentage: ", function (percent) {
+//   console.log(`${percent} was guessed.\n`);
+//   rl.close();
+// });
