444d736337c88f220fae321b75eba68bde5d937e
Brittney Trinh <brittneytrinh@g.ucla.edu>
2023-07-26 12:10:07 -0700

    import readline, mandates, cities

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world");
+/*
+    index.ts
+    Javascript i/o interactive
+*/
+const readline = require("readline");
+
+const MANDATES_CSV =
+  "https://raw.githubusercontent.com/ParkingReformNetwork/mandates-map/main/map/trimmed_map_data.csv";
+const CITIES_JSON =
+  "https://raw.githubusercontent.com/ParkingReformNetwork/parking-lot-map/main/data/score-cards.json";
+
