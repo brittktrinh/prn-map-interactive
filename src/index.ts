e5d3fb3019bfa24128f855d767f028d3473f1280
Brittney Trinh <brittneytrinh@g.ucla.edu>
2023-07-27 00:14:50 -0700

    create RNG for multiple choice options

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ import * as readline from "node:readline";
 
 // const parking_csv = ("https://raw.githubusercontent.com/ParkingReformNetwork/parking-lot-map/main/data/score-cards.json");
 
+let option_a = Math.floor(Math.random() * 101);
+console.log(option_a);
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
