af11ad7acc12be7bda944dafe8fe3e330a791783
Brittney Trinh <brittneytrinh@g.ucla.edu>
2023-07-26 23:36:48 -0700

    got readline to compile and run

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,17 @@
     index.ts
     Javascript i/o interactive
 */
-// const readline = require("readline");
+import * as readline from "node:readline";
 
-// const MANDATES_CSV =
-//   "https://raw.githubusercontent.com/ParkingReformNetwork/mandates-map/main/map/trimmed_map_data.csv";
-// const CITIES_JSON =
-//   "https://raw.githubusercontent.com/ParkingReformNetwork/parking-lot-map/main/data/score-cards.json";
+// const parking_csv = ("https://raw.githubusercontent.com/ParkingReformNetwork/parking-lot-map/main/data/score-cards.json");
 
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-// rl.question("Guess the percentage: ", function (percent) {
-//   console.log(`${percent} was guessed.\n`);
-//   rl.close();
-// });
+rl.question("Does this work?\n", function (answer) {
+  console.log(answer);
+});
+
+rl.close();
