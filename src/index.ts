8e065e5ceca9757ed0220814ca1635a829bab163
Brittney Trinh <brittneytrinh@g.ucla.edu>
2023-07-27 19:04:47 -0700

    load parking data using axios

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 /*
     index.ts
-    Javascript i/o interactive
+    Javascript interactive
 */
 import * as readline from "node:readline";
+import axios from "axios";
 
-// const parking_csv = ("https://raw.githubusercontent.com/ParkingReformNetwork/parking-lot-map/main/data/score-cards.json");
+const parking_csv = axios
+  .get(
+    "https://raw.githubusercontent.com/ParkingReformNetwork/parking-lot-map/main/data/score-cards.json",
+  )
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 let option_a = Math.floor(Math.random() * 101);
 console.log(option_a);
