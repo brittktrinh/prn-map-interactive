523dbc4e10a9659f767ed0b7bb196ee3ae5d8235
Brittney Trinh <brittneytrinh@g.ucla.edu>
2023-07-28 14:24:45 -0700

    randomize city for question

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,13 @@ const parking_csv = axios
     "https://raw.githubusercontent.com/ParkingReformNetwork/parking-lot-map/main/data/score-cards.json",
   )
   .then((response) => {
+    // Store cities into an array
     let cities = Object.keys(response.data);
-    console.log(Object.keys(response.data));
+
+    // Select a city to guess the answer to
+    let selectedCity =
+      cities[Math.floor(Math.random() * (Object.keys(cities).length + 1))];
+    console.log(selectedCity);
   })
   .catch((err) => {
     console.log(err);
